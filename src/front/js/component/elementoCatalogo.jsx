61e9448d5e8925d91805a59b6a799779458cfdb5
Sergiomerizalde <sergio.merizalde.p@gmail.com>
2022-10-20 03:12:17 +0000

    Avances 19/10 vista catalogo con mapeo

--- src/front/js/component/elementoCatalogo.jsx
+++ src/front/js/component/elementoCatalogo.jsx
@@ -9,34 +9,23 @@ export const ElementoCatalogo = ({ name, description, images }) => {
       <div className="container">
         <div className="card col-4 mt-30" style={{ width: "24rem" }}>
           <img
-            src={
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRiCv5zURHVtYiHGGuU-7b_w18cX0dGBiD6BgiOKyDw&s"
-            }
+            src={images}
             className="card-img-top"
-            alt="..."
+            style={{ width: "24rem", height: "20rem" }}
+            alt="https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg"
           />
           <div className="card-body">
-            <h1 className="card-title">Titulo de la casa</h1>
-            <h6 className="card-text">
-              <h5 className="titulo-catalogo">Esta es el nombre</h5>
-              {name}
-            </h6>
-            {/* <h6 className="card-text">
-              <h5 className="titulo-catalogo">Las amenidades son</h5>
-              {amenidades}
-            </h6>
-            <h6 className="card-text">
-              <h5 className="titulo-catalogo">Estos reviews tiene el sitio</h5>
-              {reviews}
-            </h6> */}
-            <p className="card-text">
-              <h1>Fotos del sitio</h1>
-              {images}
-            </p>
-            <h6 className="card-text">
+            <p className="card-title display-4">{name}</p>
+            <div className="card-text">
               <h5 className="titulo-catalogo">Descripción</h5>
               {description}
-            </h6>
+            </div>
+            <br />
+            <div className="d-flex justify-content-end">
+              <Link to="/" className="btn btn-primary btn-sm">
+                Ver detalles
+              </Link>
+            </div>
           </div>
         </div>
       </div>
